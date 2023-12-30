e28c2ae362038c124bc3c0bbd991c0c20ba143c1
ezequiel-lima <joao.lima94@fatec.sp.gov.br>
2023-12-30 06:29:00 -0300

    Add project files.

---
+++ Conjuntos/Program.cs
@@ -0,0 +1,34 @@
+﻿Dictionary<int, int> listStudents = new Dictionary<int, int>();
+
+Console.Write("Fow many students for course A? ");
+int numberStudents = int.Parse(Console.ReadLine());
+CountRegisteredStudents(numberStudents);
+
+Console.Write("Fow many students for course B? ");
+numberStudents = int.Parse(Console.ReadLine());
+CountRegisteredStudents(numberStudents);
+
+Console.Write("Fow many students for course C? ");
+numberStudents = int.Parse(Console.ReadLine());
+CountRegisteredStudents(numberStudents);
+
+Console.WriteLine($"Total students: {listStudents.Count}");
+
+void CountRegisteredStudents(int numberStudents)
+{
+    while (numberStudents > 0)
+    {
+        int idStudent = int.Parse(Console.ReadLine());
+
+        if (!listStudents.ContainsKey(idStudent))
+        {
+            listStudents.Add(idStudent, 1);
+        }
+        else
+        {
+            listStudents[idStudent]++;
+        }
+
+        numberStudents--;
+    }
+}
